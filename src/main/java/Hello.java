facc95dbb06e662364160b98958f792aa6d6dd10
brunolena007 <brunolena007@gmail.com>
2021-09-14 14:47:45 +0100

    updated added more hello feature

---
+++ src/main/java/Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Developing hello feature");
+        System.out.println("Local repo is already published to GitHub");
+System.out.println("doing more work");
+
+    }
+}
